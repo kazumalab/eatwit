719ee35ca63561ce3ca6d982309d593d353d2ab5
kazumalab <kazuma-arashi@hotmail.co.jp>
2017-09-30 00:22:04 +0900

    fixed rubocop

--- Gemfile
+++ Gemfile
@@ -34,6 +34,6 @@ group :development do
   gem "web-console", ">= 3.3.0"
 end
 
-gem "tzinfo-data", platforms: %i[mingw mswin x64_mingw jruby]
 gem "therubyracer"
+gem "tzinfo-data", platforms: %i[mingw mswin x64_mingw jruby]
 
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,8 +2,9 @@ require "rails_helper"
 
 RSpec.describe HomeController do
   describe "#index" do
-    before { get :index  }
+    before { get :index }
 
-    it { expect(response).to have_http_status 200  }
+    it { expect(response).to have_http_status 200 }
   end
 end
+
